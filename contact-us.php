c200601472b791afddaef1d102fe7e917bf849d3
snidhedinesh <snidhe2016@gmail.com>
2025-06-25 18:59:41 +0530

    new-commit

---
+++ contact-us.php
@@ -0,0 +1,83 @@
+<?php include 'includes/header.php' ?>
+<div class="top-bar">
+    <div class="top-bar-left">
+      Home > Select Car<br>
+      <strong>Contact Us</strong>
+    </div>
+    
+  </div>
+  <section class="div-container">
+  <h2>Get in touch</h2>
+    <p>Have a question or need assistance? We’re here to help! Contact us for bookings, inquiries, or feedback. Reach out anytime, and our team will ensure you have a smooth and hassle-free experience with us. Your journey is our priority!</p>
+<div class="row">
+    <div class="col-lg-4 col-md-6 col-am-12">
+    <div class="info-box">
+        <i class="bi bi-envelope"></i>company@gmail.com</div>
+
+    </div>
+    <div class="col-lg-4 col-md-6 col-am-12">
+    <div class="info-box phone"><i class="bi bi-telephone"></i>+91 123456789</div>
+
+    </div>
+
+    <div class="col-lg-4 col-md-6 col-am-12">
+    <div class="info-box address"><i class="bi bi-geo-alt"></i>123 street B block, 4th floor</div>
+
+    </div>
+
+</div>
+  <div class="contact-section mt-4">
+  <!-- Left Side -->
+  <div class="contact-left">
+  <iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241317.11609851093!2d73.72287973147661!3d18.524564859279732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf027ce0b187%3A0xe1fcbf97ddfb4f3a!2sPune%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1616144863844!5m2!1sen!2sin"
+  width="100%"
+  height="300"
+  style="border:0; border-radius: 12px;"
+  allowfullscreen=""
+  loading="lazy"
+  referrerpolicy="no-referrer-when-downgrade">
+</iframe>
+  </div>
+
+  <!-- Right Side -->
+  <div class="contact-right">
+    
+    <div class="form-row">
+        <div>
+            <label for="">First Name</label>
+        <input type="text" placeholder="Enter First Name" />
+
+        </div>
+        <div>
+            <label for="">Last Name</label>
+        <input type="text" placeholder="Enter Last Name" />
+
+        </div>
+    </div>
+    <div class="form-row">
+        <div>
+            <label for="">Email</label>
+        <input type="email" placeholder="Enter Email" />
+
+        </div>
+        <div>
+            <label for="">Phone Number</label>
+        <input type="text" placeholder="Enter Phone Number" />
+
+        </div>
+    </div>
+    <div class="form-row">
+        <span>
+            <label for="">Message</label>
+        <textarea placeholder="Enter Your Message"></textarea>
+
+</span>
+    </div>
+    <div class="text-end">    <button class="book-btn">EXPLORE CABS</button>
+    </div>
+  </div>
+</div>
+
+  </section>
+<?php include 'includes/footer.php' ?>
